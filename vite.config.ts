6020ee112fde3c6856614f97cb32dce10fca61ee
akashvardhan02 <161361427+akashvardhan02@users.noreply.github.com>
2024-11-21 15:46:51 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/GROPRO/', // Corrected base path without spaces
+  base: '/Gropro/', // Corrected base path without spaces
   server: {
     host: true,
     port: 5173,
